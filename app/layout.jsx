2cde49b1a03204d7aa84660f828f921c7207bcbc
Ujjwal Sharma <81868153+ujjwal-sharma-112@users.noreply.github.com>
2026-02-16 13:16:50 +0530

    Add Facebook Pixel tracking script to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -77,6 +77,22 @@ export default function RootLayout({ children }) {
             __html: JSON.stringify(structuredData)
           }}
         />
+        <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2114602632618864');
+          fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=2114602632618864&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased pt-[72px]`}
